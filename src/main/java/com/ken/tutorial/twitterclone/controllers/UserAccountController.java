b3d3aa83d127fc96a7df864314d12609c125140a
kenupdate <lax.potcx1562@gmail.com>
2020-04-04 01:54:04 +0900

    modified controller class

--- src/main/java/com/ken/tutorial/twitterclone/controllers/UserAccountController.java
+++ src/main/java/com/ken/tutorial/twitterclone/controllers/UserAccountController.java
@@ -3,18 +3,18 @@ package com.ken.tutorial.twitterclone.controllers;
 import com.ken.tutorial.twitterclone.DAO.UserAccountJDBC;
 import com.ken.tutorial.twitterclone.entities.UserAccount;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-// @RequestMapping("/user-account")
+@RestController
+@RequestMapping("/user-account")
 public class UserAccountController {
     @Autowired
     private UserAccountJDBC userAccountJDBC;
 
-    // @RequestMapping(value = "/user-account-list", method = RequestMethod.GET)
+    @RequestMapping(value = "/user-account-list", method = RequestMethod.GET)
     public String list(Model model) {
         List<UserAccount> userAccountList = userAccountJDBC.getUserList();
 
@@ -22,14 +22,14 @@ public class UserAccountController {
         return "list-user-account";
     }
 
-    // @RequestMapping(value = "/new-user-account", method = RequestMethod.GET)
+    @RequestMapping(value = "/new-user-account", method = RequestMethod.GET)
     public String newUserAccount(Model model) {
         model.addAttribute("userAccount", new UserAccount());
         return "user-account-form";
     }
 
-    // @RequestMapping(value = "/add-user-account", method = RequestMethod.POST)
-    public String addUserAccount(/*@ModelAttribute("user-account")*/ UserAccount userAccount) {
+    @RequestMapping(value = "/add-user-account", method = RequestMethod.POST)
+    public String addUserAccount(@ModelAttribute("user-account") UserAccount userAccount) {
         Integer userId      = userAccount.getUserId();
         String userPassword = userAccount.getUserPassword();
         String userName     = userAccount.getUserName();
@@ -40,15 +40,15 @@ public class UserAccountController {
         return "redirect:/user-account/user-account-list";
     }
 
-    // @RequestMapping(value = "/view-user-account", method = RequestMethod.GET)
-    public String viewUserAccount(/*@RequestParam("user_id")*/ Integer userId, Model model) {
+    @RequestMapping(value = "/view-user-account", method = RequestMethod.GET)
+    public String viewUserAccount(@RequestParam("user_id") Integer userId, Model model) {
         UserAccount userAccount = userAccountJDBC.getUserAccount(userId);
         model.addAttribute("userAccount", userAccount);
         return "user-account-form";
     }
 
-    // @RequestMapping(value = "/delete-user-account", method = RequestMethod.GET)
-    public String deleteUserAccount(/*@RequestParam("user_id")*/ Integer userId, Model model) {
+    @RequestMapping(value = "/delete-user-account", method = RequestMethod.GET)
+    public String deleteUserAccount(@RequestParam("user_id") Integer userId, Model model) {
         userAccountJDBC.deleteUser(userId);
         return "redirect:/user-account/user-list";
     }
--- src/main/java/com/ken/tutorial/twitterclone/entities/UserAccount.java
+++ src/main/java/com/ken/tutorial/twitterclone/entities/UserAccount.java
@@ -1,9 +1,22 @@
 package com.ken.tutorial.twitterclone.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class UserAccount {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
     private Integer userId;
+    @Column(name = "user_password")
     private String userPassword;
+    @Column(name = "user_name")
     private String userName;
+    @Column(name = "user_email")
     private String userEmail;
 
     public void setUserId(Integer userId) {
