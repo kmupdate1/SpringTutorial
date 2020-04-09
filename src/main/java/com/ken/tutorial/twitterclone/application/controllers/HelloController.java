02fc8913854040405b62b829d80295b9a5b1ed71
Ken <lax.potcx1562@gmail.com>
2020-04-10 04:28:02 +0900

    added the html file.

--- src/main/java/com/ken/tutorial/twitterclone/application/controllers/HelloController.java
+++ src/main/java/com/ken/tutorial/twitterclone/application/controllers/HelloController.java
@@ -1,5 +1,8 @@
 package com.ken.tutorial.twitterclone.application.controllers;
 
+import com.ken.tutorial.twitterclone.domain.logic.HelloService;
+import com.ken.tutorial.twitterclone.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+    @Autowired
+    private HelloService helloService;
+    // = new HelloService(); の代わりにアノテーション'@Autowired'でワイヤリングして
+    //　インスタンスをDIコンテナからインジェクトしてくれる．
 
     @GetMapping("/hello")
     public String getHello() {
@@ -18,5 +25,17 @@ public class HelloController {
     public String postRequest(@RequestParam("text1s")String text1s, Model model) {
         model.addAttribute("sample", text1s);
         return "helloResponse";
+    }
+
+    @PostMapping("hello/db")
+    public String postDBRequest(@RequestParam("text2s")String text2s, Model model) {
+        Integer employeeId = Integer.parseInt(text2s);
+        Employee employee = helloService.findOne(employeeId);
+
+        model.addAttribute("employee_id", employee.getEmployeeId());
+        model.addAttribute("employee_name", employee.getEmployeeName());
+        model.addAttribute("employee_age", employee.getEmployeeAge());
+
+        return "helloDBResponse";
     }
 }
