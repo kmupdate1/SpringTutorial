f51ac48d9bd473698b905e3fe546a2e4d2ee3715
Ken <lax.potcx1562@gmail.com>
2020-04-11 16:53:15 +0900

    modified them

--- src/main/java/com/ken/tutorial/twitterclone/login/domain/model/SignUpForm.java
+++ src/main/java/com/ken/tutorial/twitterclone/login/domain/model/SignUpForm.java
@@ -1,11 +1,9 @@
 package com.ken.tutorial.twitterclone.login.domain.model;
 
-import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
-@Data
 public class SignUpForm {
 
     private Integer userId;
@@ -17,4 +15,52 @@ public class SignUpForm {
 
     private Integer age;
     private Boolean marriage;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Boolean getMarriage() {
+        return marriage;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setMarriage(Boolean marriage) {
+        this.marriage = marriage;
+    }
 }
